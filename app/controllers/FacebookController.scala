0159c49d79f162bd7ce52d13412b623f8024acfd
kigi <kigi.chang@gmail.com>
2016-06-15 00:30:20 +0800

    init

---
+++ app/controllers/FacebookController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import javax.inject.Inject
+
+import play.api.mvc.Controller
+
+/**
+  * Created by kigi on 6/15/16.
+  */
+class FacebookController @Inject() extends Controller {
+
+}
---
+++ app/controllers/package.scala
@@ -0,0 +1,9 @@
+import com.typesafe.config.ConfigFactory
+
+/**
+  * Created by kigi on 6/15/16.
+  */
+package object controllers {
+
+  val config = ConfigFactory.load
+}
