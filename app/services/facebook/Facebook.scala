402364f5f4af078f0ba8bf2f62f804168b97f29b
kigi <kigi.chang@gmail.com>
2016-06-21 22:45:58 +0800

    comments

--- app/services/facebook/Facebook.scala
+++ app/services/facebook/Facebook.scala
@@ -18,15 +18,56 @@ class Facebook @Inject() (ws: WSClient) {
 
   import services._
 
+  /**
+    * User Access Token longterm 版
+    *
+    * 通過 User 授權後，取得的 access token 時效比較短，
+    * 可以透過 Facebook graph api 取得時效較長的的 token.
+    * 時效為 60 天。
+    *
+    * 切記： User Access Token 不可外流。
+    */
   private var longTermAccessToken = ""
 
+  /**
+    * User Access Token，通過 User 授權後，可以取得 facebok 給的 User Access Token，
+    * 之後要取 Facebook 的資料，都需要用這組 Token。
+    *
+    * 切記： User Access Token 不可外流。
+    */
   private var accessToken = ""
 
+  /**
+    * 取得用戶的授權時，告知 Facebook 將結果傳回到我方的那個 URL，
+    * 這組 URL 會在 client 端被呼叫。
+    *
+    * 當用戶授權後，Facebook 會回傳一組 code 給這個 URL，
+    * 到時再利用這組 code 去取得 User Access Token。
+    *
+    */
   private var redirectURI = ""
 
+  /**
+    * Facebook App 的 ID，申請時會給
+    */
   val appId = config.getString("facebook.appid")
+
+  /**
+    * Facebook App 的 secret code，申請時會給，切記不可外洩。
+    */
   val secret = config.getString("facebook.secret")
+
+  /**
+    * 使用那個 Facebook Graph API 版本
+    */
   val version = config.getString("facebook.version")
+
+  /**
+    * 自組的 App Access Token, 可以透過 App Access Token 取得 Facebook 上的資料，
+    * 但是只有部分。至於可以取得那些資料，就看對方的資料的隱私等級。
+    * 要確認的話，可以登出 Facebbook 後，去瀏覽 Facebook，
+    * 可以看到的資料，就跟 App Access Token 可取得的資料差不多。
+    */
   val simpleAppToken = appId + "|" + secret
 
 
@@ -48,6 +89,11 @@ class Facebook @Inject() (ws: WSClient) {
     s"https://www.facebook.com/$version/dialog/oauth?client_id=$appId&redirect_uri=$uri"
   }
 
+  /**
+    * 取得 User Access Token, 使用 user 授權後，回傳的 code 再去取得 User Access Token。
+    *
+    * @param code user 授權後，facebook 回傳的一組 code
+    */
   def getAccessToken(code: String): Future[Option[String]] = {
     log.debug(s"redirect_uri: $redirectURI")
 
@@ -68,8 +114,11 @@ class Facebook @Inject() (ws: WSClient) {
     }
   }
 
-  //def getAccessToken(req: Request[AnyContent]) = req.getQueryString("code"). flatMap { code => getAccessToken(code) }
-
+  /**
+    * 使用一般的 User Access Token，轉換成 longterm User Access Token。
+    * 如果 longterm 的 token 失效後，不可以由原本的 token 重取一次；
+    * 必須從 user 授權的流程開始，取得 user access token 後，再取得 long-term 版。
+    */
   def getLongAccessToken(): Future[Option[String]] = {
 
     val resp = services.get(ws, s"$GraphURL/$version/oauth/access_token",
@@ -94,36 +143,62 @@ class Facebook @Inject() (ws: WSClient) {
   def get[T](url: String, parameters: (String, String)*)(implicit rds: Reads[T]): Future[Either[JsError, T]] =
     get0(url, parameters: _*) map { response => validate[T](response.json)(rds) }
 
-
+  /**
+    * 取得 facebook 粉絲頁
+    */
   def page(id: String): Future[Either[JsError, Page]] = get[Page](s"$GraphURL/$version/$id?$PageFields")
 
+  /**
+    * 取得 facebook 用戶或粉絲頁頭像資料
+    */
   def picture(id: String): Future[Either[JsError, Picture]] =
     get[Picture](s"$GraphURL/$version/$id/picture", "redirect" -> "false", "type" -> "large")
 
-
+  /**
+    * 取得 facebook 相簿
+    */
   def albums(id: String): Future[Either[JsError, Albums]] =
     get[Albums](s"$GraphURL/$version/$id/albums?$AlbumsFields")
 
+  /**
+    * Facebook Node 分頁資料
+    */
   def paging[T](url: String)(implicit rds: Reads[T]): Future[Either[JsError, T]] = get[T](url) //services.get(ws, url) map { resp => validate[T](resp.json) }
 
-  //def albumsWithURL(url: String): Future[Either[JsError, Albums]] = paging[Albums](url)
-
-
+  /**
+    * 取得 facebook 相片
+    */
   def photos(id: String): Future[Either[JsError, Photos]] =
     get[Photos](s"$GraphURL/$version/$id/photos?$PhotosFields")
 
+  /**
+    * 取得 facebook 單張相片
+    */
   def photo(id: String): Future[Either[JsError, Photo]] =
     get[Photo](s"$GraphURL/$version/$id")
 
+  /**
+    * 取得 facebook 動態
+    */
   def posts(id: String): Future[Either[JsError, Posts]] =
     get[Posts](s"$GraphURL/$version/$id/posts?$PostsFields")
 
+  /**
+    * 取得 facebook 按讚
+    */
   def likes(id: String): Future[Either[JsError, Likes]] =
     get[Likes](s"$GraphURL/$version/$id/likes?$LikesFields", "summary" -> "true")
 
+  /**
+    * 取得 facebook 用戶資料
+    */
   def user(id: String): Future[Either[JsError, User]] =
     get[User](s"$GraphURL/$version/$id?$UserFields")
 
+  /**
+    * current token information
+    * @return
+    */
   def tokenInfo(): Future[WSResponse] = {
     services.get(ws, s"$GraphURL/$version/debug_token", "input_token" -> token, "access_token" -> token)
   }
